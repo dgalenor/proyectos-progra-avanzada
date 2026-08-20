1b5b2520048127887a4cef2261798fffbf7fff20
Diego Galeno <diegocriptocoins@gmail.com>
2026-08-19 21:29:18 -0400

    funcion cargar_tablero funcionando

--- solver.py
+++ solver.py
@@ -1,15 +1,31 @@
-def pedir_nombre():
+"""def pedir_nombre():
     nombre_archivo = input("Ingresa el nombre del archivo que contiene el sudoku: ")
     return nombre_archivo
-
+"""
 def cargar_tablero(nombre_archivo):
     fichero = open(nombre_archivo)
-    print(fichero.readlines())
+    tablero = fichero.readlines()
+    TCompleto = []
+    for fila in tablero:
+        FilaAAgregar= []
+        prov = fila.strip()
+        for caracter in prov:
+            x = int(caracter)
+            FilaAAgregar.append(x)
+        TCompleto.append(FilaAAgregar)
+
+    print(TCompleto)
+    pass
+
+
+def resolver_tablero():
     pass
 
 def main():
-    puzle = pedir_nombre()
-    cargar_tablero(puzle)
+    #puzle = pedir_nombre()
+    cargar_tablero("puzle01.txt")
+    resolver_tablero()
+
 
 if __name__ == "__main__":
     main()
