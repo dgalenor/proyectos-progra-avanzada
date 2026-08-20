c14526e8b57cff5176df3f338d79970f32dcc1f5
Diego Galeno <diegocriptocoins@gmail.com>
2026-08-19 21:51:56 -0400

    funcion encontrar_vacia() funcionando correctamente

--- solver.py
+++ solver.py
@@ -1,21 +1,33 @@
-"""def pedir_nombre():
+def pedir_nombre():
     nombre_archivo = input("Ingresa el nombre del archivo que contiene el sudoku: ")
     return nombre_archivo
-"""
+
 def cargar_tablero(nombre_archivo):
     fichero = open(nombre_archivo)
     tablero = fichero.readlines()
     TCompleto = []
+    
     for fila in tablero:
         FilaAAgregar= []
         prov = fila.strip()
+        
         for caracter in prov:
             x = int(caracter)
             FilaAAgregar.append(x)
+    
         TCompleto.append(FilaAAgregar)
+    
+    return TCompleto
 
-    print(TCompleto)
-    pass
+def encontar_vacia(tablero):
+    for fila in tablero:
+        contador = 0
+        for i in range(9):
+            if fila[i] == 0:
+                return [contador, i]
+        contador += 1 
+        
+    
 
 
 def resolver_tablero():
@@ -23,8 +35,9 @@ def resolver_tablero():
 
 def main():
     #puzle = pedir_nombre()
-    cargar_tablero("puzle01.txt")
+    tablero = cargar_tablero("puzle01.txt")
     resolver_tablero()
+    print(encontar_vacia(tablero))
 
 
 if __name__ == "__main__":
